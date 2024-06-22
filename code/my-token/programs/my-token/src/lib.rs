099546fad79bebf4db21ba20c2b79937fd534082
jbettini <ashhxtm@gmail.com>
2024-06-23 00:57:09 +0200

    Exec KO - Minting right need to be in multisig Account

--- code/my-token/programs/my-token/src/lib.rs
+++ code/my-token/programs/my-token/src/lib.rs
@@ -7,34 +7,123 @@ use anchor_spl::{
     },
 };
 use mpl_token_metadata::types::DataV2;
+use std::convert::Into;
 
 declare_id!("AaDFYp7PaMSPNdtA5B1fNEKvfNLLjAF3hMVUqZVhe1BF");
 
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Invalid Args to create Multisig Account.")]
+    InvalidArgs,
+    #[msg("Approver Not autorized.")]
+    UnauthorizedApprover,
+    #[msg("Execution Not Autorized: Not enought approver.")]
+    UnauthorizedExecution,
+}
 
+#[account]
+pub struct MultisigAccount {
+    pub signers: Vec<Pubkey>,
+    pub threshold: u64,
+}
 
 #[derive(Accounts)]
-pub struct InitNFT<'info> {
-
-
+pub struct CreateMultisig<'info> {
     #[account(mut, signer)]
-    pub signer: Signer<'info>,
+    pub buyer: Signer<'info>,
+    #[account(
+        init, 
+        payer = buyer,
+        space = 1024 * 8
+    )]
+    pub multisig_account: Account<'info, MultisigAccount>,
     #[account(
         init,
-        payer = signer,
+        payer = buyer,
         mint::decimals = 0,
-        mint::authority = signer.key(),
-        mint::freeze_authority = signer.key(),
+        mint::authority = multisig_account.key(),
+    )]
+    pub mint: Account<'info, Mint>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Transaction {
+    pub buyer: Pubkey,
+    pub signers: Vec<Pubkey>,
+    pub approver: Vec<Pubkey>,
+    pub exec: bool,
+}
+#[derive(Accounts)]
+pub struct CreateTransaction<'info> {
+    pub multisig_account: Account<'info, MultisigAccount>,
+    #[account(
+        init,
+        payer = buyer,
+        space = 1024 * 8,
+        seeds = [
+            b"create-transaction",
+            buyer.key().as_ref(),
+            multisig_account.key().as_ref(),
+        ],
+        bump
+    )]
+    pub transaction: Account<'info, Transaction>,
+    #[account(mut)]
+    pub buyer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct ApproveTransaction<'info> {
+    #[account(mut, 
+        seeds = [
+            b"create-transaction",
+            buyer.key().as_ref(),
+        ],
+        bump
+    )]
+    pub transaction: Account<'info, Transaction>,
+    #[account(signer)]
+    pub approver: Signer<'info>,
+    #[account()]
+    pub buyer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ExecuteTransaction<'info> {
+
+    #[account(mut, signer)]
+	pub buyer: Signer<'info>,
+
+
+    #[account(mut)]
+    pub transaction: Account<'info, Transaction>,
+
+
+    // Signers 
+    #[account(zero, signer)]
+    multisig_account: Account<'info, MultisigAccount>,
+
+    // Minting Account
+    #[account(
+        init,
+        payer = buyer,
+        mint::decimals = 0,
+        mint::authority = multisig_account.key(),
     )]
     pub mint: Account<'info, Mint>,
 
+    // Token Holder
     #[account(
         init_if_needed,
-        payer = signer,
+        payer = buyer,
         associated_token::mint = mint,
-        associated_token::authority = signer,
+        associated_token::authority = buyer,
     )]
     pub associated_token_account: Account<'info, TokenAccount>,
 
+    // Accounts needed
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>,
@@ -42,6 +131,7 @@ pub struct InitNFT<'info> {
     
     pub token_metadata_program: Program<'info, Metadata>,
 
+    // Metadata account
     #[account(
         mut,
         seeds = [
@@ -54,68 +144,94 @@ pub struct InitNFT<'info> {
     )]
     /// CHECK:
     pub metadata_account: UncheckedAccount<'info>,
-
 }
-
-// #[account]
-// pub struct Multisig {
-//     pub owners: Vec<Pubkey>,
-//     pub threshold: u64,
-//     pub nonce: u8,
-//     pub owner_set_seqno: u32,
-// }
-
-// impl Multisig {
-//     pub fn new(ctx: Context<InitNFT>, signers: Vec<Pubkey>, threshold: u8) -> ProgramResult {
-//         if signers.is_empty() || threshold == 0 || threshold > signers.len() as u8 {
-//             return Err(ProgramError::InvalidArgument);
-//         }        
-//         let multisig: &mut Account<Multisig> = &mut ctx.accounts.multisig;
-//         multisig.signers = signers;
-//         multisig.threshold = threshold;
-//         multisig.nonce = 0;
-//         multisig.owner_set_seqno = 0;
-//         Ok(())
-//     }
-// }
-
 
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
-    pub fn init_nft(ctx: Context<InitNFT>, name: String, symbol: String, uri: String) -> Result<()> {
-        let cpi_context = CpiContext::new(
-            ctx.accounts.token_program.to_account_info(),
-            MintTo {
-                mint: ctx.accounts.mint.to_account_info(),
-                to: ctx.accounts.associated_token_account.to_account_info(),
-                authority: ctx.accounts.signer.to_account_info(),
-            },
-        );
-        mint_to(cpi_context, 1)?;
 
-        let cpi_context = CpiContext::new(
-            ctx.accounts.token_metadata_program.to_account_info(),
-            CreateMetadataAccountsV3 {
-                metadata: ctx.accounts.metadata_account.to_account_info(),
-                mint: ctx.accounts.mint.to_account_info(),
-                mint_authority: ctx.accounts.signer.to_account_info(),
-                update_authority: ctx.accounts.signer.to_account_info(),
-                payer: ctx.accounts.signer.to_account_info(),
-                system_program: ctx.accounts.system_program.to_account_info(),
-                rent: ctx.accounts.rent.to_account_info(),
-            },
-        );
-        let data_v2 = DataV2 {
-            name,
-            symbol,
-            uri,
-            seller_fee_basis_points: 0,
-            creators: None,
-            collection: None,
-            uses: None,
-        };
-        create_metadata_accounts_v3(cpi_context, data_v2, false, true, None)?;
-        Ok(())
+    pub fn new_multisig(
+        ctx: Context<CreateMultisig>, 
+        signers: Vec<Pubkey>
+        ) -> Result<()> {
+            // Rendre les signer unique
+            if signers.is_empty() && signers.len() < 3 {
+                return Err(ErrorCode::InvalidArgs.into());
+            }
+            let multisig: &mut Account<MultisigAccount> = &mut ctx.accounts.multisig_account;
+            multisig.signers = signers;
+            multisig.threshold = 3;
+            Ok(())
+    }
+
+    pub fn new_transaction(
+        ctx: Context<CreateTransaction>, 
+        buyer: Pubkey, 
+        signers: Vec<Pubkey>
+        ) -> Result<()> {
+            let transaction = &mut ctx.accounts.transaction;
+            transaction.buyer = buyer;
+            transaction.signers = signers;
+            transaction.approver = Vec::new();
+            transaction.exec = false;
+            Ok(())
+    }
+
+    pub fn approve_transaction(ctx: Context<ApproveTransaction>) -> Result<()> {
+        let transaction = &mut ctx.accounts.transaction;
+        if transaction.signers.contains(&ctx.accounts.approver.key) && !transaction.approver.contains(&ctx.accounts.approver.key) {
+            transaction.approver.push(*ctx.accounts.approver.key);
+            if transaction.approver.len() >= 3  {
+                transaction.exec = true;
+            }
+            Ok(())
+        } else {
+            return Err(ErrorCode::UnauthorizedApprover.into())
+        }
+    }
+
+    pub fn execute(
+        ctx: Context<ExecuteTransaction>,         
+        name: String, 
+        symbol: String, 
+        uri: String
+        ) -> Result<()> {
+            let transaction = &mut ctx.accounts.transaction;
+            if !transaction.exec {
+                return Err(ErrorCode::UnauthorizedExecution.into());
+            }
+            let cpi_context = CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    mint: ctx.accounts.mint.to_account_info(),
+                    to: ctx.accounts.associated_token_account.to_account_info(),
+                    authority: ctx.accounts.multisig_account.to_account_info(),
+                },
+            );
+            mint_to(cpi_context, 1)?;
+            // Add Metadata
+            let cpi_context = CpiContext::new(
+                ctx.accounts.token_metadata_program.to_account_info(),
+                CreateMetadataAccountsV3 {
+                    metadata: ctx.accounts.metadata_account.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                    mint_authority: ctx.accounts.multisig_account.to_account_info(),
+                    update_authority: ctx.accounts.multisig_account.to_account_info(),
+                    payer: ctx.accounts.buyer.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+            );
+            let data_v2 = DataV2 {
+                name,
+                symbol,
+                uri,
+                seller_fee_basis_points: 0,
+                creators: None,
+                collection: None,
+                uses: None,
+            };
+            create_metadata_accounts_v3(cpi_context, data_v2, false, true, None)?;
+            Ok(())
     }
 }
--- code/my-token/tests/my-token.ts
+++ code/my-token/tests/my-token.ts
@@ -2,11 +2,15 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaNftAnchor } from "../target/types/solana_nft_anchor";
 import { getAssociatedTokenAddress } from "@solana/spl-token";
-import { MPL_TOKEN_METADATA_PROGRAM_ID } from "@metaplex-foundation/mpl-token-metadata";
-import {
-	TOKEN_PROGRAM_ID,
-	ASSOCIATED_TOKEN_PROGRAM_ID,
-} from "@solana/spl-token";
+// import { MPL_TOKEN_METADATA_PROGRAM_ID } from "@metaplex-foundation/mpl-token-metadata";
+// import {
+// 	TOKEN_PROGRAM_ID,
+// 	ASSOCIATED_TOKEN_PROGRAM_ID,
+// } from "@solana/spl-token";
+
+const tx = (name: string, tx: string) => {
+	console.log(`${name}: https://explorer.solana.com/tx/${tx}?cluster=devnet\n`);
+}
 
 function randomIntFromInterval(min, max): number {
 	return Math.floor(Math.random() * (max - min + 1) + min);
@@ -33,44 +37,91 @@ describe("solana-nft-anchor", async () => {
 	// Configured the client to use the devnet cluster.
 	const provider = anchor.AnchorProvider.env();
 	anchor.setProvider(provider);
-	const program = anchor.workspace
-		.SolanaNftAnchor as Program<SolanaNftAnchor>;
-	const signer = provider.wallet;
-	const mint = anchor.web3.Keypair.generate();
+	const program = anchor.workspace.SolanaNftAnchor as Program<SolanaNftAnchor>;
+	const buyer = provider.wallet as anchor.Wallet;
 
-	// Derive the associated token address account for the mint
-	const associatedTokenAccount = await getAssociatedTokenAddress(
-		mint.publicKey,
-		signer.publicKey
-	);
-
-	const metadata = {
-		name: "MonkeyArt4200",
-		symbol: "MKA",
-		uri: getUri(),
-	};
-
+	// test si exec sans signature - Avec une signature qui a pas le droit - plusieur fois avec la meme signature
 	it("mints nft!", async () => {
-		const tx = await program.methods
-			.initNft(metadata.name, metadata.symbol, metadata.uri)
-			.accounts({
-				signer: provider.publicKey,
-				mint: mint.publicKey,
-				associatedTokenAccount,
-				tokenProgram: TOKEN_PROGRAM_ID,
-				associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
-				tokenMetadataProgram: MPL_TOKEN_METADATA_PROGRAM_ID,
-				systemProgram: anchor.web3.SystemProgram.programId,
-				rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-			})
-			.signers([mint])
-			.rpc();
+		const multisigKeys = new Array(3).fill(null).map(() => anchor.web3.Keypair.generate());
+		const signerPubkeys = multisigKeys.map(key => key.publicKey);
 
-		console.log(
-			`mint nft tx: https://explorer.solana.com/tx/${tx}?cluster=devnet`
+		// create multisig
+		const multisigAccount =  anchor.web3.Keypair.generate();
+		tx('Create Mulsig Account', await program.methods
+			.newMultisig(signerPubkeys)
+			.accounts({
+				buyer: buyer.publicKey,
+				multisigAccount: multisigAccount.publicKey,
+			})
+			.signers([multisigAccount])
+			.rpc());
+
+		// // // propose transac
+		const [transaction] = anchor.web3.PublicKey.findProgramAddressSync(
+			[
+				Buffer.from("Transaction-Account"),
+				buyer.publicKey.toBuffer(),
+				multisigAccount.publicKey.toBuffer(),
+			],
+			program.programId,
 		);
-		console.log(
-			`minted nft: https://explorer.solana.com/address/${mint.publicKey}?cluster=devnet`
+		
+		tx('Create Transaction', await program.methods
+			.newTransaction(
+				buyer.publicKey,
+				signerPubkeys,
+			)
+			.accounts({
+				buyer: buyer.publicKey,
+				multisigAccount: multisigAccount.publicKey,
+			})
+			.signers([buyer.payer])
+			.rpc());
+
+		// // approve transaction
+
+		const approveTransaction = async (approver: anchor.web3.Keypair) =>{
+			tx(`Approve Transaction`, await program.methods
+				.approveTransaction()
+				.accounts({
+					approver: approver.publicKey,
+					buyer: buyer.publicKey,
+					multisigAccount: multisigAccount.publicKey,
+				})
+				.signers([approver])
+				.rpc());
+		};
+		
+		const metadata = {
+			name: "MonkeyArt4242",
+			symbol: "MKA",
+			uri: getUri(),
+		};
+
+		const mint = anchor.web3.Keypair.generate();
+		const associatedTokenAccount = await getAssociatedTokenAddress(
+			mint.publicKey,
+			buyer.publicKey
 		);
+
+		const executeTransaction = async () => {
+			tx(`Execute Transaction`, await program.methods
+				.execute(metadata.name, metadata.symbol, metadata.uri)
+				.accounts({
+					buyer: buyer.publicKey,
+					transaction: transaction,
+					multisigAccount: multisigAccount.publicKey,
+					associatedTokenAccount: associatedTokenAccount,
+				})
+				.signers([buyer.payer])
+				.rpc());
+		};
+
+		// Accept the transaction
+		for (const owner of multisigKeys.slice(0)) {
+			await approveTransaction(owner);
+		}
+
+		await executeTransaction();
 	});
 });

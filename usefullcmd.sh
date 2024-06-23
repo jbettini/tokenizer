e1befb6715dcbfc4388f656f39e681527468c1b9
jbettini <ashhxtm@gmail.com>
2024-06-23 17:41:31 +0200

    edit dockerfile + set usefullcmd

---
+++ usefullcmd.sh
@@ -0,0 +1,13 @@
+
+
+# solana config get/
+
+docker-compose run --rm tokenizer /bin/bash
+
+anchor test --skip-build --skip-deploy
+
+solana program show [PROGRAM ID]
+solana program extend [PROGRAM ID] 100000
+
+yarn install
+yarn add --dev ts-mocha typescript @types/node @types/mocha && yarn add @metaplex-foundation/umi @metaplex-foundation/umi-bundle-defaults  @metaplex-foundation/umi-web3js-adapters @metaplex-foundation/mpl-token-metadata@v3.0.0-alpha.24 @metaplex-foundation/umi-signer-wallet-adapters @solana/spl-token

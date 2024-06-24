af28554f6c90348d40f8bab7819a12285a6d385b
jbettini <ashhxtm@gmail.com>
2024-06-25 01:16:59 +0200

    Patch Cargo.lock

--- documentation/usefullcmd.sh
+++ documentation/usefullcmd.sh
@@ -7,7 +7,7 @@ anchor test --skip-build --skip-deploy
 solana program show |PROGRAM ID|
 solana program extend |PROGRAM ID| 100000
 
-yarn add --dev ts-mocha typescript @types/node @types/mocha && \
+yarn add --dev ts-mocha typescript @types/node @types/mocha && 
 yarn add @solana/spl-token @metaplex-foundation/umi@^0.8.2 @solana/web3.js@^1.91.6 fastestsmallesttextencoderdecoder@^1.0.22
 
 docker system prune -a --volumes
